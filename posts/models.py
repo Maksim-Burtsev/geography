94081586bf9913e69350f52d2a56d2b5f2597e87
MaximBurtsev <zadrot-lol@list.ru>
2022-03-20 19:42:40 +0300

    add posts

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,19 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     """Категории публикаций"""
     name = models.CharField('Название', max_length=255)
+    photo = models.ImageField('Фотография', upload_to='cat_photos', blank=True)
 
     def __str__(self) -> str:
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_pk': self.pk})
 
     class Meta:
         verbose_name = 'Категория'
@@ -17,14 +23,17 @@ class Category(models.Model):
 class Post(models.Model):
     """Публикация"""
     title = models.CharField('Название', max_length=255)
-    
+
+    text = models.TextField('Тест публикации')
+
     summary = models.TextField('Текст на главной', blank=True)
 
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                verbose_name='Автор', related_name='posts')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
                                  null=True, verbose_name='Категория', related_name='posts')
-    photo = models.ImageField('Фотография', upload_to='post_photos')
+    photo = models.ImageField(
+        'Фотография', upload_to='post_photos', blank=True, null=True)
 
     created_time = models.DateTimeField('Дата создания', auto_now_add=True)
 
@@ -36,6 +45,10 @@ class Post(models.Model):
     def __str__(self) -> str:
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_pk': self.pk})
+
     class Meta:
         verbose_name = 'Публикация'
         verbose_name_plural = 'Публикации'
+        ordering = ['-created_time']
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
 
+from posts.models import Post, Category
+
 
 def index(request):
-    return render(request, 'posts/index.html')
+    """Главная страница"""
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'posts/index.html', context)
+
+
+def post(request, post_pk):
+    """Страница публикации"""
+
+    post = Post.objects.get(pk=post_pk)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'posts/post.html', context)
+
+def all_posts(request):
+    """Страница со всеми статьями"""
+
+    categories = Category.objects.all()
+
+    context = {
+        'categories' : categories,
+    }
+
+    return render(request, 'posts/all_posts.html', context)
+
+def show_category(request, cat_pk):
+    """Все публикации конкретной категории"""
+    category = Category.objects.get(pk=cat_pk)
+
+    context = {
+        'category':category,
+    }
+
+    return render(request, 'posts/category.html', context)
