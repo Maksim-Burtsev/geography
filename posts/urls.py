5a2812d2e8bb7cf13404df5375e831f73c72cefe
MaximBurtsev <zadrot-lol@list.ru>
2022-03-20 19:42:40 +0300

    add posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 
-from posts.views import index
+from posts.views import index, post, all_posts, show_category
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='home'),
+    path('post/<int:post_pk>', post, name='post'),
+    path('all_posts', all_posts, name='posts'),
+    path('category/<int:cat_pk>', show_category, name='category')
 ]
