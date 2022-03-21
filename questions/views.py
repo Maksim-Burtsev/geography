df3709b336ea8b7b7ff92558b274a251629f4dc7
MaximBurtsev <zadrot-lol@list.ru>
2022-03-21 22:22:26 +0300

    questions app updates

--- questions/views.py
+++ questions/views.py
@@ -1,4 +1,24 @@
+from importlib.metadata import requires
 from django.shortcuts import render
 
+from questions.models import Question, Answer, Test
+
 def index_questions(request):
-    pass
+    """Страница со всеми тестами"""
+    tests = Test.objects.all()
+    context = {'tests':tests,}
+    
+    return render(request, 'questions/all_tests.html', context)
+
+def show_test(request, test_pk):
+    """Страница теста"""
+
+    if request.method == 'POST':
+        print(request.POST)
+
+    test = Test.objects.get(pk=test_pk)
+
+    context = {'test':test}
+
+    return render(request, 'questions/show_test.html', context)
+    
