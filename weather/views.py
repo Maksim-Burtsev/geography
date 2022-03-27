3f43ee486255df001286bea46754949005991973
MaximBurtsev <zadrot-lol@list.ru>
2022-03-27 23:02:30 +0300

    add try/except and weather app and brought function in other file

--- weather/views.py
+++ weather/views.py
@@ -1,10 +1,6 @@
-import os
 
 from django.shortcuts import render
-from weather.services import get_city_image_url
-
-from dotenv import load_dotenv
-from pyowm import OWM
+from weather.services import get_city_image_url, get_city_temp
 
 
 def get_weather(request):
@@ -13,32 +9,20 @@ def get_weather(request):
     if request.GET.get('city'):
         city = request.GET.get('city')
 
-        load_dotenv()
-
-        api_key = os.getenv('API_KEY')
-
-        owm = OWM(api_key)
-        mgr = owm.weather_manager()
-
         try:
-            observation = mgr.weather_at_place(city)
+            temp, temp_feel = get_city_temp(city)
+
         except Exception as e:
             print(e)
+
         else:
-            w = observation.weather
-
-            temp_json = w.temperature('celsius')
-
-            temp = temp_json.get('temp')
-            temp_feel = temp_json.get('feels_like')
-
             try:
                 image_url = get_city_image_url(city)
             except Exception as e:
                 print(e)
-            else:    
+            else:
                 context['image_url'] = image_url
-            
+            print(image_url)
             context['temp'] = round(temp)
             context['temp_feel'] = round(temp_feel)
             context['city'] = city
